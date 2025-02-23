51fb314081ffb215e6eaf1d89398dc8db6cd96f9
Ed Cragg <ed.cragg@eipi.xyz>
2025-02-23 11:21:10 +0000

    Retab header

--- TimerFour.h
+++ TimerFour.h
@@ -44,59 +44,59 @@ class TimerFour
     //  Configuration
     //****************************
     void initialize(unsigned long microseconds=1000000) __attribute__((always_inline)) {
-	TCCR4B = _BV(WGM43);        // set mode as phase and frequency correct pwm, stop the timer
-	TCCR4A = 0;                 // clear control register A
-	setPeriod(microseconds);
+        TCCR4B = _BV(WGM43);        // set mode as phase and frequency correct pwm, stop the timer
+        TCCR4A = 0;                 // clear control register A
+        setPeriod(microseconds);
     }
     void setPeriod(unsigned long microseconds) __attribute__((always_inline)) {
-    	/* WGM43 mode goes from BOTTOM to TOP (ICR4) then from TOP to BOTTOM, then from BOTTOM to TOP.
-    	 * OVF interrupt is triggered on TOP, thus the period covers 2*ICR4 counts.
-    	 * Max period is 8'388'480 µs
-    	 */
-    	const unsigned long cycles = (F_CPU / 2000000) * microseconds;
-	if (cycles < TIMER4_RESOLUTION) {
-		clockSelectBits = _BV(CS40);
-		pwmPeriod = cycles;
-	} else
-	if (cycles < TIMER4_RESOLUTION * 8) {
-		clockSelectBits = _BV(CS41);
-		pwmPeriod = cycles / 8;
-	} else
-	if (cycles < TIMER4_RESOLUTION * 64) {
-		clockSelectBits = _BV(CS41) | _BV(CS40);
-		pwmPeriod = cycles / 64;
-	} else
-	if (cycles < TIMER4_RESOLUTION * 256) {
-		clockSelectBits = _BV(CS42);
-		pwmPeriod = cycles / 256;
-	} else
-	if (cycles < TIMER4_RESOLUTION * 1024) {
-		clockSelectBits = _BV(CS42) | _BV(CS40);
-		pwmPeriod = cycles / 1024;
-	} else {
-		clockSelectBits = _BV(CS42) | _BV(CS40);
-		pwmPeriod = TIMER4_RESOLUTION - 1;
-	}
-	ICR4 = pwmPeriod;
-	TCCR4B = _BV(WGM43) | clockSelectBits;
+        /* WGM43 mode goes from BOTTOM to TOP (ICR4) then from TOP to BOTTOM, then from BOTTOM to TOP.
+         * OVF interrupt is triggered on TOP, thus the period covers 2*ICR4 counts.
+         * Max period is 8'388'480 µs
+         */
+        const unsigned long cycles = (F_CPU / 2000000) * microseconds;
+        if (cycles < TIMER4_RESOLUTION) {
+                clockSelectBits = _BV(CS40);
+                pwmPeriod = cycles;
+        } else
+        if (cycles < TIMER4_RESOLUTION * 8) {
+                clockSelectBits = _BV(CS41);
+                pwmPeriod = cycles / 8;
+        } else
+        if (cycles < TIMER4_RESOLUTION * 64) {
+                clockSelectBits = _BV(CS41) | _BV(CS40);
+                pwmPeriod = cycles / 64;
+        } else
+        if (cycles < TIMER4_RESOLUTION * 256) {
+                clockSelectBits = _BV(CS42);
+                pwmPeriod = cycles / 256;
+        } else
+        if (cycles < TIMER4_RESOLUTION * 1024) {
+                clockSelectBits = _BV(CS42) | _BV(CS40);
+                pwmPeriod = cycles / 1024;
+        } else {
+                clockSelectBits = _BV(CS42) | _BV(CS40);
+                pwmPeriod = TIMER4_RESOLUTION - 1;
+        }
+        ICR4 = pwmPeriod;
+        TCCR4B = _BV(WGM43) | clockSelectBits;
     }
 
     //****************************
     //  Run Control
     //****************************
     void start() __attribute__((always_inline)) {
-	TCCR4B = 0;
-	TCNT4 = 0;		// TODO: does this cause an undesired interrupt?
-	resume();
+        TCCR4B = 0;
+        TCNT4 = 0;              // TODO: does this cause an undesired interrupt?
+        resume();
     }
     void stop() __attribute__((always_inline)) {
-	TCCR4B = _BV(WGM43);
+        TCCR4B = _BV(WGM43);
     }
     void restart() __attribute__((always_inline)) {
-	start();
+        start();
     }
     void resume() __attribute__((always_inline)) {
-	TCCR4B = _BV(WGM43) | clockSelectBits;
+        TCCR4B = _BV(WGM43) | clockSelectBits;
     }
 
     //****************************
@@ -106,55 +106,55 @@ class TimerFour
      * Duty goes from 0 to 1023.
      */
     void setPwmDuty(char pin, unsigned int duty) __attribute__((always_inline)) {
-	unsigned long dutyCycle = pwmPeriod;
-	dutyCycle *= duty;
-	dutyCycle >>= 10;
-	if (pin == TIMER4_A_PIN) OCR4A = dutyCycle;
-	#ifdef TIMER4_B_PIN
-	else if (pin == TIMER4_B_PIN) OCR4B = dutyCycle;
-	#endif
-	#ifdef TIMER4_C_PIN
-	else if (pin == TIMER4_C_PIN) OCR4C = dutyCycle;
-	#endif
+        unsigned long dutyCycle = pwmPeriod;
+        dutyCycle *= duty;
+        dutyCycle >>= 10;
+        if (pin == TIMER4_A_PIN) OCR4A = dutyCycle;
+        #ifdef TIMER4_B_PIN
+        else if (pin == TIMER4_B_PIN) OCR4B = dutyCycle;
+        #endif
+        #ifdef TIMER4_C_PIN
+        else if (pin == TIMER4_C_PIN) OCR4C = dutyCycle;
+        #endif
     }
     void pwm(char pin, unsigned int duty) __attribute__((always_inline)) {
-	if (pin == TIMER4_A_PIN) { pinMode(TIMER4_A_PIN, OUTPUT); TCCR4A |= _BV(COM4A1); }
-	#ifdef TIMER4_B_PIN
-	else if (pin == TIMER4_B_PIN) { pinMode(TIMER4_B_PIN, OUTPUT); TCCR4A |= _BV(COM4B1); }
-	#endif
-	#ifdef TIMER4_C_PIN
-	else if (pin == TIMER4_C_PIN) { pinMode(TIMER4_C_PIN, OUTPUT); TCCR4A |= _BV(COM4C1); }
-	#endif
-	setPwmDuty(pin, duty);
-	TCCR4B = _BV(WGM43) | clockSelectBits;
+        if (pin == TIMER4_A_PIN) { pinMode(TIMER4_A_PIN, OUTPUT); TCCR4A |= _BV(COM4A1); }
+        #ifdef TIMER4_B_PIN
+        else if (pin == TIMER4_B_PIN) { pinMode(TIMER4_B_PIN, OUTPUT); TCCR4A |= _BV(COM4B1); }
+        #endif
+        #ifdef TIMER4_C_PIN
+        else if (pin == TIMER4_C_PIN) { pinMode(TIMER4_C_PIN, OUTPUT); TCCR4A |= _BV(COM4C1); }
+        #endif
+        setPwmDuty(pin, duty);
+        TCCR4B = _BV(WGM43) | clockSelectBits;
     }
     void pwm(char pin, unsigned int duty, unsigned long microseconds) __attribute__((always_inline)) {
-	if (microseconds > 0) setPeriod(microseconds);
-	pwm(pin, duty);
+        if (microseconds > 0) setPeriod(microseconds);
+        pwm(pin, duty);
     }
     void disablePwm(char pin) __attribute__((always_inline)) {
-	if (pin == TIMER4_A_PIN) TCCR4A &= ~_BV(COM4A1);
-	#ifdef TIMER4_B_PIN
-	else if (pin == TIMER4_B_PIN) TCCR4A &= ~_BV(COM4B1);
-	#endif
-	#ifdef TIMER4_C_PIN
-	else if (pin == TIMER4_C_PIN) TCCR4A &= ~_BV(COM4C1);
-	#endif
+        if (pin == TIMER4_A_PIN) TCCR4A &= ~_BV(COM4A1);
+        #ifdef TIMER4_B_PIN
+        else if (pin == TIMER4_B_PIN) TCCR4A &= ~_BV(COM4B1);
+        #endif
+        #ifdef TIMER4_C_PIN
+        else if (pin == TIMER4_C_PIN) TCCR4A &= ~_BV(COM4C1);
+        #endif
     }
 
     //****************************
     //  Interrupt Function
     //****************************
     void attachInterrupt(void (*isr)()) __attribute__((always_inline)) {
-	isrCallback = isr;
-	TIMSK4 = _BV(TOIE4);
+        isrCallback = isr;
+        TIMSK4 = _BV(TOIE4);
     }
     void attachInterrupt(void (*isr)(), unsigned long microseconds) __attribute__((always_inline)) {
-	if(microseconds > 0) setPeriod(microseconds);
-	attachInterrupt(isr);
+        if(microseconds > 0) setPeriod(microseconds);
+        attachInterrupt(isr);
     }
     void detachInterrupt() __attribute__((always_inline)) {
-	TIMSK4 = 0;
+        TIMSK4 = 0;
     }
     static void (*isrCallback)();
 
